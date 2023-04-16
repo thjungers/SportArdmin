92e39724cb4b61309a3a77cfd780b65c434fdda2
thjungers <thomasjungers@gmail.com>
2023-04-16 16:09:08 +0000

    Create seeder
    Seeds tables sections, sessions, users, section_user, registrations, user_sessions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Section;
+use App\Models\Session;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\VarDumper\VarDumper;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create Sections and their Sessions
+        $num_sessions = 10;
+        $sections = Section::factory()
+                            ->count(5)
+                            ->sequence(
+                                ['name' => 'Badminton', 'is_free' => false],
+                                ['name' => 'Jogging',   'is_free' => true ],
+                                ['name' => 'Natation',  'is_free' => false],
+                                ['name' => 'Yoga',      'is_free' => false],
+                                ['name' => 'Vélo',      'is_free' => true ],
+                            )
+                            ->hasSessions($num_sessions)
+                            ->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create Users and their Registrations and attach the Users to their Sections
+        $users = User::factory(10)
+                    ->hasAttached(
+                        fake()->randomElements($sections, fake()->numberBetween(1, 4)),
+                        ['expires_on' => '2023-08-31']
+                    )
+                    ->hasRegistrations()
+                    ->create();
+
+        // Attach Users to the Sessions corresponding to their Sections
+        foreach ($users as $user) {
+            foreach ($user->sections as $section) {
+                $user->sessions()->attach(
+                    $section->sessions->random(fake()->numberBetween(0, $num_sessions))
+                );
+                // The first session may be free
+                $first_session = $user->sessions()->orderBy('starts_at')->first();
+                if($first_session !== null)
+                {
+                    $first_session->pivot->is_free = fake()->boolean();
+                    $first_session->pivot->save();
+                }
+            }
+        }
     }
 }
